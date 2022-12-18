77d9b966cf7e26c7e2754f3f8f9cb901af6bd1da
TriTran951 <thanhtritran951@gmail.com>
2022-12-18 12:19:00 +0700

    Chỉnh lại scene và thêm hình background cả map

--- 05-SceneManager/Font.h
+++ 05-SceneManager/Font.h
@@ -1,24 +1,16 @@
 #pragma once
+#include"Sprites.h"
+#include <map>
+#include "AssetIDs.h"
 
-#include "GameObject.h"
-#include "Sprites.h"
-#include "Game.h"
-#include "Textures.h"
-
-#define ID_TEXT_FONT 110
-#define NUM_OF_LETTER 26
-#define NUM_OF_NUMBER 10
-#define TILE_FONT_WIDTH 8
-#define	TILE_FONT_LETTER_HEIGHT 8
-#define TILE_FONT_NUMBER_HEIGHT 8
-#define SPACE_WIDTH 8
-#define NOTE_OF_EXCLAMATION_WIDTH 6
-#define TOP_LETTER 0
 class CFont
 {
+	std::map<char, LPSPRITE> fonts;
+
 public:
-	LPTEXTURE Tex;
-	void Draw(float x, float y, const string& s);
 	CFont();
+	void Init();
+	LPSPRITE mapping(char c);
+	vector<LPSPRITE> StringToSprite(string str);
 };
 
--- 05-SceneManager/PlayScene.h
+++ 05-SceneManager/PlayScene.h
@@ -6,19 +6,25 @@
 #include "Brick.h"
 #include "Mario.h"
 #include "Goomba.h"
-//#include "Koopas.h"
+#include "Map.h"
 
 
+#define WORLD_1			0
+#define WORLD_INTRO		6
+#define WORLD_1_1		1
+#define WORLD_1_4		4
+
+#define CAM_CHANGE_TIME		30
 class CPlayScene: public CScene
 {
 protected: 
 	// A play scene has to have player, right? 
 	LPGAMEOBJECT player;					
-
+	CMap* current_map = NULL;
 
 	void _ParseSection_SPRITES(string line);
 	void _ParseSection_ANIMATIONS(string line);
-
+	void _ParseSection_TILEMAP_DATA(string line);
 	void _ParseSection_ASSETS(string line);
 	void _ParseSection_OBJECTS(string line);
 
@@ -33,9 +39,8 @@ public:
 	virtual void Update(DWORD dt);
 	virtual void Render();
 	virtual void Unload();
-
+	CMap* GetMap() { return current_map; }
 	LPGAMEOBJECT GetPlayer() { return player; }
-
 	void Clear();
 	void PurgeDeletedObjects();
 
