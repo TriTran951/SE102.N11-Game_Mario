0cecfe2934f474f795f2df9efb2bd754f6d61b39
TriTran951 <90551445+TriTran951@users.noreply.github.com>
2023-01-28 20:51:55 +0700

    fix logic map + cơ chế render camera

--- 05-SceneManager/BrickColor.cpp
+++ 05-SceneManager/BrickColor.cpp
@@ -8,17 +8,19 @@
 
 void CBrickColor::Render()
 {
+	if (!checkObjectInCamera(this)) return;
+
 	CAnimations* animations = CAnimations::GetInstance();
 	animations->Get(ID_ANI_BRICK_COLOR)->Render(x, y);
-	//RenderBoundingBox();
+	RenderBoundingBox();
 }
 
 void CBrickColor::GetBoundingBox(float& l, float& t, float& r, float& b)
 {
 	l = x - BRICK_BBOX_WIDTH / 2;
-	t = y - BRICK_BBOX_HEIGHT / 2;
+	t = y - BRICK_BBOX_HEIGHT /2;
 	r = l + BRICK_BBOX_WIDTH;
-	b = t + BRICK_BBOX_HEIGHT;
+	b = t + float( BRICK_BBOX_HEIGHT / 1.5);
 }
 void CBrickColor::Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects) {
 	if (!checkObjectInCamera(this)) return;
